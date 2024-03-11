ea23abb8e70d44b127fb841c58c4ddf79247a8ba
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-11 13:14:16 +0300

    upgrade signal handler connector helper

--- app/common/signals.py
+++ app/common/signals.py
@@ -1,8 +1,25 @@
-from typing import Callable, cast
+from typing import Callable, TypeVar, Union
 
-from PyQt6.QtCore import pyqtSignal, pyqtBoundSignal
+from PyQt6.QtCore import pyqtBoundSignal, pyqtSignal
+
+SignalType = TypeVar("SignalType", bound=Union[pyqtSignal, pyqtBoundSignal])
 
 
-def connect_handler_to_signal(signal: pyqtSignal, handler: Callable) -> None:
-    signal_ = cast(pyqtBoundSignal, signal)
-    signal_.connect(handler)
+def connect_handler_to_signal(
+    signal: SignalType,
+    handler: Callable[[int], None],
+) -> None:
+    """
+    PyCharm doesn't recognise signals as `pyqtBoundSignal`,
+    it thinks signals are of type `pyqtSignal`.
+    This func handles types for stat-analysis (mypy, etc.).
+
+    :param signal:
+    :param handler:
+    :return:
+    """
+    if isinstance(signal, pyqtBoundSignal):
+        signal.connect(handler)
+
+    msg = f"Signal must be of type `pyqtBoundSignal`, got {type(signal)}"
+    raise TypeError(msg)
