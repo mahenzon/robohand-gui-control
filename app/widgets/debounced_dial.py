14c9656f6032aff2cb9922e26b666a4371c780a5
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-11 13:41:28 +0300

    create debounced dial widget

---
+++ app/widgets/debounced_dial.py
@@ -0,0 +1,39 @@
+from typing import Callable
+
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QDial
+
+from app.common.signals import connect_handler_to_signal
+from app.widgets.debounced import Debounced
+
+
+class DebouncedDial(QDial):  # type: ignore[misc]
+    changed = pyqtSignal(int)
+
+    def __init__(
+        self,
+        notches_visible: bool = True,
+        wrapping: bool = False,
+        min_value: int = -90,
+        max_value: int = 90,
+    ) -> None:
+        super().__init__()
+        self.setNotchesVisible(notches_visible)
+        self.setWrapping(wrapping)
+        self.setRange(min_value, max_value)
+
+        self.debounce = Debounced()
+
+        connect_handler_to_signal(
+            self.valueChanged,
+            self.debounce.handle,
+        )
+
+    def add_debounced_handler(self, handler: Callable[[int], None]) -> None:
+        """
+        Your callback handler
+
+        :param handler:
+        :return:
+        """
+        connect_handler_to_signal(self.debounce.signal, handler)
