5f9066294d33fcaa8a3c19e7b09bea6c2dc9e07c
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-11 13:37:17 +0300

    create LCD value indicator widget

---
+++ app/widgets/lcd_value_indicator_widget.py
@@ -0,0 +1,21 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QLabel, QLCDNumber, QVBoxLayout, QWidget
+
+
+class LCDValueIndicatorWidget(QWidget):  # type: ignore[misc]
+    def __init__(
+        self,
+        label_text: str,
+        label_max_height: int = 20,
+        lcd_min_height: int = 60,
+    ) -> None:
+        super().__init__()
+        layout = QVBoxLayout()
+        label = QLabel(label_text)
+        label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        label.setMaximumHeight(label_max_height)
+        layout.addWidget(label)
+        self.lcd = QLCDNumber()
+        self.lcd.setMinimumHeight(lcd_min_height)
+        layout.addWidget(self.lcd)
+        self.setLayout(layout)
