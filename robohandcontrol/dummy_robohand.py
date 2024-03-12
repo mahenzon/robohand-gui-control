348f4a2e6eadbafbe8f1984d52410279c2126c3c
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-12 23:31:13 +0300

    Create logged RoboHand control

---
+++ robohandcontrol/dummy_robohand.py
@@ -0,0 +1,22 @@
+import logging
+
+from robohandcontrol.robocontrol import RobohandControlBase
+
+log = logging.getLogger(__name__)
+
+
+class LoggedRobohandControl(RobohandControlBase):
+    def control_claw(self, angle: int) -> None:
+        log.info("Set claw angle to %s", angle)
+
+    def control_extend_arrow(self, angle: int) -> None:
+        log.info("Extend tower to angle %s", angle)
+
+    def control_raise_arrow(self, angle: int) -> None:
+        log.info("Raise tower to angle %s", angle)
+
+    def control_rotation(self, angle: int) -> None:
+        log.info("Rotate base to angle %s", angle)
+
+    def set_led_rgb(self, red: int, green: int, blue: int) -> None:
+        log.info("Set led rgb to %s, %s, %s", red, green, blue)
