ca3cfb55bf49c9eb74d1c162718e6c8236448505
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-13 16:55:11 +0300

    Add common config for socket commands

---
+++ config.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+SERVER_IP_CONNECT = "127.0.0.1"
+SERVER_IP_BIND = "0.0.0.0"  # noqa: S104
+SERVER_PORT = 12345
+
+COMMAND_SPLITTER = "|"
+
+
+class ControlParam(str, Enum):
+    CLAW = "claw"
+    EXTEND_ARROW = "extend_arrow"
+    RAISE_ARROW = "raise_arrow"
+    ROTATION = "rotation"
+    LED = "led"
