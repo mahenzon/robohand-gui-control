3fa03f0eeac61dc96098d7200930e85567c58fa2
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-15 21:41:17 +0300

    create combined robocontrol window

---
+++ app/widgets/combined_robocontrol_window.py
@@ -0,0 +1,78 @@
+import logging
+import sys
+
+from PySide6.QtCore import QModelIndex, Qt
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QSplitter, QWidget
+
+import config
+from app.common.mappings import NAMES_TO_CONTROL_PARAMS
+from app.common.robohand_getter import robohand_control
+from app.widgets.robocontol_predefined_commands import (
+    RoboControlPredefinedCommandsWidget,
+)
+from app.widgets.robocontrol import RoboControlWindow
+from robohandcontrol.robocontrol import RobohandControlBase
+
+
+class CombinedRoboControlWindow(QWidget):
+    def __init__(self, robohand: RobohandControlBase) -> None:
+        super().__init__()
+
+        self.robo_control = RoboControlWindow(robohand)
+
+        self.predefined_commands = RoboControlPredefinedCommandsWidget()
+        self.splitter = QSplitter(self)
+
+        self.splitter.addWidget(self.robo_control)
+        self.splitter.addWidget(self.predefined_commands)
+
+        self.main_layout = QHBoxLayout()
+
+        self.main_layout.addWidget(self.splitter)
+        self.setLayout(self.main_layout)
+
+        self.register_actions()
+
+        self.setWindowTitle("RI RoboHand Control")
+        self.setGeometry(400, 300, 1200, 600)
+
+    def register_actions(self) -> None:
+        self.predefined_commands.add_button.clicked.connect(
+            self.handle_save_current_state,
+        )
+        self.predefined_commands.list_view.doubleClicked.connect(
+            self.handle_command_double_clicked,
+        )
+
+    def get_current_state_as_commands_text(self) -> str:
+        states = []
+        for name, indicator in self.robo_control.indicators_panel.indicators.items():
+            value = indicator.lcd.value()
+            command = NAMES_TO_CONTROL_PARAMS[name]
+            states.append(config.COMMAND_SPLITTER.join((command, str(int(value)))))
+        # add endl
+        states.append("")
+        commands_text = config.COMMAND_ENDL.join(states)
+        return commands_text
+
+    def handle_save_current_state(self) -> None:
+        commands_text = self.get_current_state_as_commands_text()
+        self.predefined_commands.add_element(commands_text)
+
+    def handle_command_double_clicked(self, index: QModelIndex) -> None:
+        model = self.predefined_commands.string_list_model
+        command_value = model.data(index, Qt.DisplayRole)  # type: ignore[attr-defined]
+        self.robo_control.set_state_from_commands(command_value)
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    app = QApplication(sys.argv)
+    robohand = robohand_control()
+    main_window = CombinedRoboControlWindow(robohand)
+    main_window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
