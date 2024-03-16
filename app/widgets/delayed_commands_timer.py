6ac53a36dfa05d080e9516173a9cfa1a6d5a72f0
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-16 11:22:04 +0300

    create delayed commands timer

---
+++ app/widgets/delayed_commands_timer.py
@@ -0,0 +1,48 @@
+import logging
+from typing import Callable
+
+from PySide6.QtCore import Signal, QTimer
+from PySide6.QtWidgets import QWidget
+
+import config
+
+log = logging.getLogger(__name__)
+
+
+class DelayedCommandsTimer(QWidget):
+    timer_signal = Signal(int)
+    timer_finished = Signal()
+
+    def __init__(
+        self,
+        handler: "Callable[[str], None]",
+        timeout: int = config.COMMANDS_TIMEOUT,
+    ):
+        super().__init__()
+        self.timeout = timeout
+        self.handler = handler
+        self.index = 0
+        self.timer = QTimer()
+        self.commands = []
+        self.timer_finished.connect(self.reset)
+        # noinspection PyUnresolvedReferences
+        self.timer.timeout.connect(self.send_command)
+
+    def start(self) -> None:
+        log.info("Start commands timer w/ commands %s", self.commands)
+        self.timer.start(self.timeout)
+
+    def reset(self, commands: "list[str]" = None):
+        self.timer.stop()
+        self.index = 0
+        self.commands = commands or []
+
+    def send_command(self):
+        self.timer_signal.emit(self.index)
+        self.handler(self.commands[self.index])
+
+        self.index += 1
+
+        if self.index >= len(self.commands):
+            self.timer.stop()
+            self.timer_finished.emit()
